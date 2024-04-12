6e96bd18168feb0bba73424b25c6431de7834fb5
dir-FY24nakajimayoudai <166081906+dir-FY24nakajimayoudai@users.noreply.github.com>
2024-04-12 01:44:31 +0000

    名前を変更してコメントを追加しました

--- forGithubCodespace/src/practice/Sample.java
+++ forGithubCodespace/src/practice/Sample.java
@@ -1,12 +1,16 @@
 package practice;
 
+// 標準ライブラリからのインポート
 import java.util.Arrays;
 
-public class Main {
+// メインファイルの内容
+public class Sample {
 
+    // 実行されるメインメソッド
+    // 実行する際は画面右上の▶アイコンをクリック
     public static void main(String[] args) {
 
-
+        //3x4の２次元配列の各要素に代入して適当に出力
         int[][] multiArray = new int[3][4];
 
         for(int i = 0; i < multiArray.length; i ++){
