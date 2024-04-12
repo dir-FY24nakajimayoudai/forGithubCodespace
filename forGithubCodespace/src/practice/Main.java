5f11ae8ffe2fbc2a0ccb1cfccda040384a309430
dir-FY24nakajimayoudai <166081906+dir-FY24nakajimayoudai@users.noreply.github.com>
2024-04-12 01:20:02 +0000

    サンプルコードを記載しました

--- forGithubCodespace/src/practice/Main.java
+++ forGithubCodespace/src/practice/Main.java
@@ -3,7 +3,14 @@ package practice;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        int[][] multiArray = new int[3][4];
+
+        for(int i = 0; i < multiArray.length; i ++){
+            for(int j = 0; j < multiArray[i].length; j ++){
+                multiArray[i][j] = i + j + 1;
+            }   
+        }
+        System.out.println(multiArray[2][3]);
 
     }
 
